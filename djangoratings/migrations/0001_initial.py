b34412aef70b4608716d7a71dd3a53ae281c9f7f
clincher <va.bolshakov@gmail.com>
2014-06-06 22:23:31 +0800

    import added

--- djangoratings/migrations/0001_initial.py
+++ djangoratings/migrations/0001_initial.py
@@ -4,6 +4,14 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
+try:
+    from django.contrib.auth import get_user_model
+except ImportError: # django < 1.5
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
 user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
 user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
 user_ptr_name = '%s_ptr' % User._meta.object_name.lower()
